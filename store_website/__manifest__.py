8b7c1faa3588c491aa2290511c2225d7de3baba9
istauski-orinix <i.stauski@orinix.tech>
2025-09-10 15:46:40 +0300

    added qweb templates and scss styles for them in store_web catalog

---
+++ store_website/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'store_web',
+    'version': '1.0',
+    'category': 'Website',
+    'depends': ['website'],
+    'data': [
+        "views/store_website_assets.xml",
+        "views/store_website_templates.xml",
+    ],
+    'installable': True,
+    'application': False,
+}
